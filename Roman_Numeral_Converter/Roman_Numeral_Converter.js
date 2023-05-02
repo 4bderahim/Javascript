b0da3f6a96245a44b09c0ea0a2ae72b1d9e703c2
4bderahim <88629357+4bderahim@users.noreply.github.com>
2023-05-03 00:35:50 +0100

    Update Roman_Numeral_Converter.js

--- Roman_Numeral_Converter/Roman_Numeral_Converter.js
+++ Roman_Numeral_Converter/Roman_Numeral_Converter.js
@@ -2,7 +2,6 @@
 let snum = [1000,100,10,1]
 let g = "";
 function name(num) {
-    console.log(num);
     snum = typeof num === "number" ? snum.splice(4 - num.toString().split("").length):snum;
     let numm = typeof num === "number" ? num.toString().split(""): num;
     let obj = {1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
